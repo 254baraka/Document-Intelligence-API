a753976eabf25640ff26a296dfadd59645cffb74
Your Name <ibrahimkhalif196@gmail.com>
2026-07-02 12:28:23 +0300

    removed embeddig

--- app/vectorstore.py
+++ app/vectorstore.py
@@ -3,7 +3,7 @@ from chromadb.utils import embedding_functions
 
 client = chromadb.PersistentClient(path="./data/chroma_db")
 
-embedding_fn = embedding_functions. SentenceTransformerEmbeddingFunction(model_name = "all-MiniLM-L6-v2")
+# embedding_fn = embedding_functions. SentenceTransformerEmbeddingFunction(model_name = "all-MiniLM-L6-v2")
 
 # collection -vin chromadb is like a table in databse 
 collection = client.get_or_create_collection(name="documents")
